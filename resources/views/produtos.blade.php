f00eb51b6f8120724311648ac73e02f7e321b297
tiago <tiagolemespalhano@gmail.com>
2022-05-11 01:21:01 -0300

    94. HTML - View Produto - Tabela de Produtos

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,5 +1,33 @@
 @extends('layout.app', ["current" => "produtos" ])
 
 @section('body')
-    <h4>Página de produtos</h4>
+<div class="card border">
+  <div class="card-body">
+    <h5 class="card-title">Cadastro de Produtos</h5>
+
+
+
+    <table class="table table-ordered table-hover">
+      <thead>
+        <tr>
+          <th>Cod</th>
+          <th>Nome</th>
+          <th>Qtd</th>
+          <th>Preço</th>
+          <th>Dpto</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+
+              
+      </tbody>
+    </table>
+
+       
+  </div>
+  <div class="card-footer">
+    <button class="btn btn-sm btn-primary" role="button">Novo Produto</button>
+  </div>
+</div>
 @endsection
